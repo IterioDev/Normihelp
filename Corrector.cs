18862493135bec85f676c3bf9ece7042b7cc4711
Iterio <iterio@iteriostuff.xyz>
2022-08-07 02:02:46 +0200

    Add Corrector base

---
+++ Corrector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normihelp
+{
+    public static class Corrector
+    {
+        public static Dictionary<string, Action<int, int>> CorrectionHandlers = new Dictionary<string, Action<int, int>>();
+    }
+}
